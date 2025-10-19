caff189c0cf64e9bb0d2e3cbbecc31bbedb97f2b
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2025-10-19 09:07:55 +0600

    refactor

--- src/main/java/com/googlecalender/GoogleCalenderApplication.java
+++ src/main/java/com/googlecalender/GoogleCalenderApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoogleCalenderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GoogleCalenderApplication.class, args);
     }
-
-
 }
