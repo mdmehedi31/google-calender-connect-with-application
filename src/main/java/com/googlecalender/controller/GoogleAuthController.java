da8681e88eb0ad0afea68da10446057f3e1e931c
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2025-10-19 08:31:35 +0600

    refactor code

--- src/main/java/com/googlecalender/controller/GoogleAuthController.java
+++ src/main/java/com/googlecalender/controller/GoogleAuthController.java
@@ -1,10 +1,8 @@
 package com.googlecalender.controller;
 
 
-import com.google.api.client.auth.oauth2.Credential;
 import com.googlecalender.service.CalenderService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
