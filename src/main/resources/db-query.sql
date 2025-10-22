401849d550fdecacfc351cd1bb4bbdb7a7ba3485
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2025-10-22 09:45:53 +0600

    sql user

--- src/main/resources/db-query.sql
+++ src/main/resources/db-query.sql
@@ -1,3 +1,13 @@
 
 -- Create db sql--
 CREATE SCHEMA `calendar_connect_app`;
+
+-- Create user table SQL ---
+CREATE TABLE `calendar_connect_app`.`tb_user` (
+          `id` INT NOT NULL AUTO_INCREMENT,
+          `email` VARCHAR(300) NULL,
+          `user_name` VARCHAR(300) NULL,
+          `google_refresh_token` VARCHAR(300) NULL,
+          `google_access_token` LONGTEXT NULL,
+          `google_token_expired_at` INT NULL,
+          PRIMARY KEY (`id`));
